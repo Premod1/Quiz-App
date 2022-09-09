8b58a510aaa1d598746111fc29364c4d807c37c1
Premod1 <premodsuraweera1@gmail.com>
2022-09-09 22:20:44 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>Math Quiz App</h1>
+    <OperatorQuiz @onBack="clearOperator" v-if="operator" :operator="operator"/>
+    <OperatorSelector v-if="!operator" @changeOperator="changeOperator"/>
+   
+   
+  </div>
+  
+</template>
+
+<script>
+import OperatorSelector from "./components/OperatorSelector.vue"  
+import OperatorQuiz from "./components/OperatorQuiz.vue"
+export default {
+    name: "app",
+    data() {
+      return {
+        operator: null,
+      }
+    },
+    methods: {
+      changeOperator(operator) {
+        this.operator = operator
+      },
+      clearOperator() {
+        this.operator = null;
+      }
+    },
+    components: {
+      OperatorSelector,
+      OperatorQuiz,
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif ;
+  -webkit-font-smoothing: antialiased;
+  -moz-osz-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}  
+button {
+  padding: 33px;
+  margin: 5px;
+  width: 175px;
+  height: 95px;
+  font-size: 22px;
+  background-color: #10aeb2;
+  color: white;
+}
+</style>
---
+++ src/components/OperatorSelector.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>  
+    <button @click="$emit('changeOperator', '+')">Addition <br>+</button>
+    <button @click="$emit('changeOperator', '-')">Subtraction<br> -</button>
+    <button @click="$emit('changeOperator', '/')">Division<br> /</button>
+    <button @click="$emit('changeOperator', '*')">Multiphication<br> *</button> 
+</div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
